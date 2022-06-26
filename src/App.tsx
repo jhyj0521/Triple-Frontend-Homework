58bfff5669216c861dd9abdf64ef6fd790018d8f
jhyj0521 <jhyj3636@gmail.com>
2022-06-27 05:33:43 +0900

    conf: styled-components 설치와 필요 없는 파일 정리
    styled-components 라이브러리를 설치했습니다.
    불필요한 기본 파일들을 정리했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import { useState } from 'react'
+import styled from 'styled-components'
 
-import logo from './logo.svg'
-import './App.css'
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: blue;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return <Container>hello</Container>
 }
 
 export default App
