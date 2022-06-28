c70818adbd24376ab094590736c7bbd00f82a986
jhyj0521 <jhyj3636@gmail.com>
2022-06-28 20:06:17 +0900

    refactor: Statistic 컴포넌트 리팩토링
    Statistic 컴포넌트 내부에 선언한 변수를 좀 더 일관성있고, 가독성 좋은 변수명으로 변경했습니다.
    statistics 데이터를 별도의 배열 변수로 분리하였습니다.

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -1,54 +1,53 @@
-import { useState } from 'react'
 import styled from 'styled-components'
 
 import useCountUp from '../hooks/useCountUp'
 
-const Ul = styled.ul`
+const statistics = [
+  { id: 0, count: 350, unit: '명', target: '사용자' },
+  { id: 1, count: 21, unit: '개', target: '리뷰' },
+  { id: 2, count: 650, unit: '개', target: '저장' },
+]
+
+const StyledStatistic = styled.ul`
   margin: 0 0 0 670px;
   padding-top: 150px;
 `
 
 const Statistic = () => {
-  const [statistics] = useState([
-    { id: 0, count: 350, unit: '명', target: '사용자' },
-    { id: 1, count: 21, unit: '개', target: '리뷰' },
-    { id: 2, count: 650, unit: '개', target: '저장' },
-  ])
-
   return (
-    <Ul>
+    <StyledStatistic>
       {statistics.map(({ id, count, unit, target }) => (
         <Statistic.Item key={id} count={count} unit={unit} target={target} />
       ))}
-    </Ul>
+    </StyledStatistic>
   )
 }
 
-interface Props {
+interface ItemProps {
   count: number
   unit: string
   target: string
 }
 
-const Li = styled.li`
+const StyledItem = styled.li`
   font-size: 36px;
   letter-spacing: -1px;
   margin-bottom: 20px;
   color: ${({ theme }) => theme.colors.grey};
 `
 
-const Item = ({ count, unit, target }: Props) => {
+const Item = ({ count, unit, target }: ItemProps) => {
   const { count: num } = useCountUp({ end: count })
 
   return (
-    <Li>
+    <StyledItem>
       <span>
         <strong>
           {num}만 {unit}
         </strong>
         의 {target}
       </span>
-    </Li>
+    </StyledItem>
   )
 }
 
