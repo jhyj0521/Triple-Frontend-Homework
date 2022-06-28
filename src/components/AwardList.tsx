7e5681c07b281250fd2c8b59acce45e97934e2b6
jhyj0521 <jhyj3636@gmail.com>
2022-06-28 19:55:06 +0900

    refactor: AwardList 컴포넌트 리팩토링
    AwardList 컴포넌트 내부에 선언한 변수를 좀 더 일관성있고, 가독성 좋은 변수명으로 변경했습니다.
    Award 데이터를 별도의 배열 변수로 분리하였습니다.

--- src/components/AwardList.tsx
+++ src/components/AwardList.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
 import styled from 'styled-components'
 
-const Ul = styled.ul`
+const awards = [
+  {
+    id: 0,
+    awardImgSrc: 'assets/play-store2x.png',
+    description: '2018 구글 플레이스토어 \n올해의 앱 최우수상 수상',
+  },
+  {
+    id: 1,
+    awardImgSrc: 'assets/badge-apple4x.png',
+    description: '2018 애플 앱스토어 \n오늘의 여행앱 선정',
+  },
+]
+
+const StyledAwardList = styled.ul`
   margin: 50px 0px 140px 670px;
   white-space: pre-wrap;
   display: flex;
 `
 
 const AwardList = () => {
-  const [awards] = useState([
-    {
-      id: 0,
-      awardImgSrc: 'assets/play-store2x.png',
-      description: '2018 구글 플레이스토어 \n올해의 앱 최우수상 수상',
-    },
-    {
-      id: 1,
-      awardImgSrc: 'assets/badge-apple4x.png',
-      description: '2018 애플 앱스토어 \n오늘의 여행앱 선정',
-    },
-  ])
-
   return (
-    <Ul>
+    <StyledAwardList>
       {awards.map(({ id, awardImgSrc, description }) => (
         <AwardList.Item
           key={id}
@@ -30,33 +29,33 @@ const AwardList = () => {
           description={description}
         />
       ))}
-    </Ul>
+    </StyledAwardList>
   )
 }
 
-interface Props {
+interface ItemProps {
   awardImgSrc: string
   description: string
 }
 
-const StyledAward = styled.li<Pick<Props, 'awardImgSrc'>>`
+const StyledItem = styled.li<Pick<ItemProps, 'awardImgSrc'>>`
   height: 54px;
   padding: 5px 0px 5px 62px;
-  font-size: 14px;
-  line-height: 22px;
   margin-right: 39px;
   background-image: url(${({ awardImgSrc }) => awardImgSrc});
   background-size: 54px 54px;
   background-position: left top;
   background-repeat: no-repeat;
-  color: ${({ theme }) => theme.colors.greyOpacity800};
+  font-size: 14px;
   font-weight: bold;
+  line-height: 22px;
+  color: ${({ theme }) => theme.colors.greyOpacity800};
 `
 
-const Award = ({ awardImgSrc, description }: Props) => {
-  return <StyledAward awardImgSrc={awardImgSrc}>{description}</StyledAward>
+const Item = ({ awardImgSrc, description }: ItemProps) => {
+  return <StyledItem awardImgSrc={awardImgSrc}>{description}</StyledItem>
 }
 
-AwardList.Item = Award
+AwardList.Item = Item
 
 export default AwardList
